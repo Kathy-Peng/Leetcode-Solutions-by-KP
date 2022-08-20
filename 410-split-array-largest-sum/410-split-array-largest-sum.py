c4763cb62b25ae41ae71b6ef4edcff614d8c28f2
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-08-19 21:57:17 -0400

    Time: 48 ms (25.91%), Space: 13.4 MB (83.94%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def helper(self, nums, mid, m):
+        subarray = 0
+        currSum = 0
+        for num in nums:
+            currSum += num
+            if currSum > mid:
+                subarray +=1
+                currSum = num
+        return subarray+1 <= m
+        
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        l,  r = max(nums), sum(nums)
+        res = r
+        while l <= r:
+            mid = (l + r) // 2
+            if self.helper(nums, mid, m):
+                res = mid
+                r = mid-1
+            else:
+                l = mid+1
+        return res
