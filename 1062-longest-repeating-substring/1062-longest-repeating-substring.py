0a9af657676adbf266423b2f3e2a1fff6dd55ca3
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-08-19 20:37:49 -0400

    Time: 50 ms (65.63%), Space: 14.7 MB (50.00%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -29,7 +29,8 @@ class Solution(object):
         l = 0
         r = len(s)-1
         while l < r:
-            mid = l + (r-l+1) //2 
+            # mid = l + (r-l+1) //2 
+            mid = (l+r+1)//2
             if self.repeat(s,mid):
                 l = mid
             else:
