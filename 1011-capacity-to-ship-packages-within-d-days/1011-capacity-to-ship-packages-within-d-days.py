14ae01cc98c2ddb9e2c0053952c3d219c3e5bcb8
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-08-23 15:30:07 -0400

    Time: 726 ms (31.54%), Space: 15.6 MB (10.07%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def helper(self, weights, mid, days):
+        substring = 1
+        currSum = 0
+        for item in weights:
+            if currSum + item > mid:
+                substring += 1
+                currSum = item
+            else:
+                currSum += item
+        return substring <= days
+    
+    def shipWithinDays(self, weights, days):
+        """
+        :type weights: List[int]
+        :type days: int
+        :rtype: int
+        """
+        l = max(weights)
+        r = sum(weights)
+        while l < r:
+            mid = (l + r) / 2
+            print(mid, self.helper(weights, mid, days))
+            if self.helper(weights, mid, days):
+                r = mid
+            else:
+                l = mid + 1
+        return l
