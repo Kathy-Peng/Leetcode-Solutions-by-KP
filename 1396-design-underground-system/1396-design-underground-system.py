fd9a133dbc111547de7250ec43063346605193fc
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-09-27 20:08:46 -0400

    Time: 560 ms (11.16%), Space: 27.3 MB (22.68%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,8 +1,8 @@
 class UndergroundSystem(object):
 
     def __init__(self):
-        self.customer_database = {}
-        self.station_database = {}
+        self.arrival_info = {}
+        self.total = {}
 
     def checkIn(self, id, stationName, t):
         """
@@ -11,8 +11,9 @@ class UndergroundSystem(object):
         :type t: int
         :rtype: None
         """
-        customer_entry = {stationName: t}
-        self.customer_database[id] = customer_entry
+        #when checking in customers we only update the arrival_info table
+        #we store the stationName and checkin time with the customer id as key
+        self.arrival_info[id] = [stationName,t]
         
     def checkOut(self, id, stationName, t):
         """
@@ -21,24 +22,14 @@ class UndergroundSystem(object):
         :type t: int
         :rtype: None
         """
-        customer_entry = self.customer_database[id]
-        checkin_station, checkin_time = customer_entry.items()[0]
-        duration = t - checkin_time
-        if checkin_station in self.station_database:
-            entry = self.station_database[checkin_station]
-            if stationName in entry:
-                #if someone has finished the same journey before
-                time_list = entry[stationName]
-                time_list.append(duration)
-                entry[stationName]=time_list
-                self.station_database[checkin_station] = entry
-            else:
-                #if previous customers have completed journeys starting from the same checkinStation but no one completed a journey to the checkout station
-                entry[stationName] = [duration]
+        checkin_station = self.arrival_info[id][0]
+        checkin_time = self.arrival_info[id][1]
+        if (checkin_station,stationName) not in self.total:
+            self.total[(checkin_station,stationName)] = [t-checkin_time, 1]
         else:
-            #no one ever complete a journey involving either checkin-station and checkout-station
-            entry = {stationName: [duration]}
-            self.station_database[checkin_station] = entry
+            entry = self.total[(checkin_station,stationName)]
+            new_entry = [entry[0]+(t-checkin_time), entry[1]+1]
+            self.total[(checkin_station,stationName)] = new_entry
             
     def getAverageTime(self, startStation, endStation):
         """
@@ -46,15 +37,11 @@ class UndergroundSystem(object):
         :type endStation: str
         :rtype: float
         """
+        entry = self.total[(startStation,endStation)]
+        total_time = entry[0]
+        count = entry[1]
+        return float(total_time)/count
         
-        entry = self.station_database[startStation]                
-        time_list = entry[endStation]
-        print(time_list)
-        sum = 0
-        for item in time_list:
-            sum += item
-        
-        return float(sum) / len(time_list)
     
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
