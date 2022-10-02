d9870daab4d599b083f115b54895c81108356cec
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-10-01 23:18:48 -0400

    Time: 56 ms (8.47%), Space: 15.6 MB (25.25%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -9,12 +9,23 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        if not head:
-            return None
-        if not head.next:
-            return head
-        prev = self.reverseList(head.next)
-        #we make head.next points to head itself, reversing the direction
-        head.next.next = head
-        head.next = None
+        #two pointer method: prev, curr
+        #as we traverse the linked list, we make curr points to prev
+        #and then iterate the process until we hit the end of curr, return curr
+        
+        #step1: initialize two pointers prev and curr
+        prev = None
+        curr = head
+        #step 2: go into for loop and make curr points to prev
+        while curr:
+            #store a tmp for next node
+            tmp = curr.next
+            #actual reversing step
+            curr.next = prev
+            #update both curr and prev
+            prev = curr
+            curr = tmp
+        #step 3: make dummy head points to curr and return as result
         return prev
+            
+            
