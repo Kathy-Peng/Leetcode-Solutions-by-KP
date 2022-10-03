58bd411cf8e6b988d87c4b2e0f5d2376781d5d69
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-10-03 16:52:33 -0400

    Time: 124 ms (25.20%), Space: 16.7 MB (71.43%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -4,10 +4,9 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: bool
         """
-        times = set()
-        for item in intervals:
-            for i in range(item[0], item[1]):
-                if i in times:
-                    return False
-                times.add(i)
+        intervals.sort()
+        for i in range(len(intervals)-1):
+            if intervals[i][1]>intervals[i+1][0]:
+                return False
         return True
+            
