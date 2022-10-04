5928f9635b2c4db9307c159cdb350db416bc8b00
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-10-04 19:03:02 -0400

    Time: 91 ms (65.01%), Space: 16.8 MB (85.50%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -0,0 +1,32 @@
+
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        start = []
+        end = []
+        for item in intervals:
+            start.append(item[0])
+            end.append(item[1])
+        start.sort()
+        end.sort()
+        l1, l2 = 0,0
+        max_count, count = 0,0 
+        while l1<len(intervals):
+            if start[l1]<end[l2]:
+                count += 1
+                if count > max_count:
+                    max_count = count
+                l1 += 1
+            else: 
+                #if one meeting starts at the same time another meeting ends
+                #we don't count them as overlapping and decrement the meeting
+                count -= 1
+                l2 += 1
+        return max_count
+                
+            
+            
+                
