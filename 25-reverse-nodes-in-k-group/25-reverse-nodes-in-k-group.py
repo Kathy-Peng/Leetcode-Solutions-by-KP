46688362cf59e1a253e8f885a59358b8ce20b035
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-10-04 16:50:09 -0400

    Time: 32 ms (96.98%), Space: 15.3 MB (30.89%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -26,12 +26,16 @@ class Solution(object):
             kth = helper(groupPrev, k) 
             if not kth:
                 break
+            #we need a new variable to reference this node after kth
+            #otherwise if we later modify where kth is pointing to this kth.next 
+            #will also be changed into something else
             groupNext = kth.next
             prev = groupNext
             while curr!=groupNext:
                 tmp = curr.next
                 curr.next = prev
                 prev, curr = curr, tmp
+            #update where current groupPrev is pointing and update what groupPrev should be next
             newgroupprev = groupPrev.next 
             groupPrev.next = kth
             groupPrev = newgroupprev
