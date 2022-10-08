1bcb14cfe7e1a9d0b0a2f7abb4c617e816cd1325
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-10-07 20:51:58 -0400

    Time: 225 ms (6.90%), Space: 14.9 MB (26.58%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def helper(self, graph, stack, partial_result, results):
+            while len(stack)!=0:
+                curr = stack.pop()
+                if curr==len(graph)-1:
+                    partial_copy = partial_result[:]
+                    partial_copy.append(curr)
+                    results.append(partial_copy)
+                else:
+                    for child in graph[curr]:
+                        partial_copy = partial_result[:]
+                        partial_copy.append(curr)
+                        self.helper(graph,[child], partial_copy, results)
+
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        results = []
+        stack = graph[0]
+        self.helper(graph, stack, [0], results)
+        return results
+       
