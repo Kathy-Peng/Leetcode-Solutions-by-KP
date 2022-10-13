feb3bf8e0764b49dcc395ddea0e23564c9150ecf
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-10-12 22:06:58 -0400

    Time: 604 ms (76.85%), Space: 16.7 MB (23.03%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,40 @@
+from queue import PriorityQueue
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        #step 1: create adjacency matrix from times
+        adj_max = {}
+        for item in times:
+            if item[0] in adj_max:
+                curr = adj_max[item[0]]
+                curr.append([item[1], item[2]])
+                adj_max[item[0]]=curr
+            else:
+                adj_max[item[0]]=[[item[1], item[2]]]
+        #step 2: initialize the priority queue and visited_set
+        pq = PriorityQueue()
+        pq.put((0, k))
+        visited = {k:0}
+        while not pq.empty():
+            prev_cost, node = pq.get()
+            visited[node]=prev_cost
+            if len(visited)==n:
+                return prev_cost
+            if node not in adj_max:
+                continue
+            for item in adj_max[node]:
+                neighbor = item[0]
+                curr_cost = item[1]
+                total_cost = prev_cost + curr_cost
+                if neighbor not in visited:
+                    pq.put((total_cost, neighbor))
+        return -1
+                    
+                    
+                
+                
+            
+            
+            
+        
+        
+        
