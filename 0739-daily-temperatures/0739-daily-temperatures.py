5be51df912d18b01ec7f0ddcbef034d23ad5de0c
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-10-23 21:53:56 -0400

    Time: 4022 ms (5.03%), Space: 27.5 MB (10.21%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,14 +2,19 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         answer = []
         stack = []
+        #step 1: traverse the stack backwards
         for i in range(len(temperatures)-1, -1, -1):
+            #step 2: keep popping elements from stack until it's empty or the top of stack is larger than the current element
             while len(stack)!=0 and temperatures[stack[-1]]<=temperatures[i]:
                 stack.pop()
+            #step 3: if the stack ends up empty then there are no larger elements
             if len(stack)==0:
                 answer.append(0)
             else:
+            #step 4: if found a larger element, record the distance between them
                 answer.append(stack[-1]-i)
             stack.append(i)
+        #step 5: since we traverse backwards, we need to reverse the answer
         answer = reversed(answer)
         return answer
             
