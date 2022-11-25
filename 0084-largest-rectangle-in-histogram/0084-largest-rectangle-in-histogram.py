2624dc38ad77093fb3139c7b0112af8e369dd86a
Kathy-Peng <55202091+Kathy-Peng@users.noreply.github.com>
2022-11-25 17:00:41 -0500

    Time: 1297 ms (80.25%), Space: 30 MB (38.39%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -3,15 +3,13 @@ class Solution:
         max_area = 0
         stack = [] #tuple of (idx, height)
         for idx, height in enumerate(heights):
-            if not len(stack):
-                stack.append((idx, height))
-            if stack[-1][1] <= height:
-                stack.append((idx, height))
-            else:
-                while stack and stack[-1][1] > height:
-                    prev_idx, prev_height = stack.pop()
-                    max_area = max(max_area, (idx-prev_idx)*prev_height)
-                stack.append((prev_idx, height))
+            start = idx
+            #if there is a dip, calculate max_area with all the heights larger than the dipped height
+            while stack and stack[-1][1] > height:
+                prev_idx, prev_height = stack.pop()
+                max_area = max(max_area, (idx-prev_idx)*prev_height)
+                start = prev_idx
+            stack.append((start, height))
                     
         for idx, height in stack:
             max_area = max(max_area, (len(heights) - idx) * height)
